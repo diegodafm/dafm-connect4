f8f5eb15540cfed02fc5c31eb226d0f675206ea9
Diego Mendonca <u06dxm@macys.com>
2015-01-06 19:37:53 -0200

    implementing basic functionality

--- src/scripts/controllers/GameController.js
+++ src/scripts/controllers/GameController.js
@@ -4,7 +4,17 @@
 angular.module('connect4')
     .controller('GameController', function($scope) {
 
+        $scope.player1 = {
+            ready: false
+        };
+
+        $scope.player2 = {
+            ready: false
+        };
+
         $scope.matrix = [];
+
+        $scope.currentPlayer = null;
 
         for (var i = 6 - 1; i >= 0; i--) {
             $scope.matrix[i] = [];
@@ -18,11 +28,13 @@ angular.module('connect4')
         }
 
         $scope.checkAvailability = function(line, column) {
-            for (var i = 6 - 1; i >= 0; i--) {
-                if (i === line) {
-                    for (var j = 7 - 1; j >= 0; j--) {
-                        if (j === column) {
-                            return $scope.matrix[i][j].available;
+            if ($scope.isReady()) {
+                for (var i = 6 - 1; i >= 0; i--) {
+                    if (i === line) {
+                        for (var j = 7 - 1; j >= 0; j--) {
+                            if (j === column) {
+                                $scope.updateLowestPiece(line, column);
+                            }
                         }
                     }
                 }
@@ -30,12 +42,56 @@ angular.module('connect4')
         };
 
         $scope.updateLowestPiece = function(line, column) {
-            for (var i = 6 - 1; i >= 0; i--) {
+            for (var i = $scope.matrix.length - 1; i >= line; i--) {
                 for (var j = 7 - 1; j >= 0; j--) {
                     if (j === column) {
-
+                        if ($scope.matrix[i][j].available) {
+                            $scope.matrix[i][j].player = $scope.getCurrentPlayer();
+                            $scope.matrix[i][j].available = false;
+                            $scope.switchPlayer();
+                            return;
+                        }
                     }
                 }
+            }
+        };
+
+        $scope.isReady = function() {
+            if ($scope.player1.ready && $scope.player2.ready) {
+                return true;
+            }
+        };
+
+        $scope.validatePlayer = function(player) {
+            if (player.nickname !== '' && player.color !== '') {
+                player.ready = true;
+            } else {
+                player.errorMessage = 'Please inform your nickname and select your color';
+                player.hasError = true;
+            }
+
+            if ($scope.isReady()) {
+                $scope.startGame();
+            }
+        };
+
+        $scope.startGame = function() {
+            if ((Math.floor(Math.random() * 2) + 1) === 1) {
+                $scope.currentPlayer = $scope.player2;
+            } else {
+                $scope.currentPlayer = $scope.player1;
+            }
+        };
+
+        $scope.getCurrentPlayer = function() {
+            return $scope.currentPlayer;
+        };
+
+        $scope.switchPlayer = function() {
+            if ($scope.currentPlayer === $scope.player1) {
+                $scope.currentPlayer = $scope.player2;
+            } else {
+                $scope.currentPlayer = $scope.player1;
             }
         };
 
--- src/scripts/directives/piece.js
+++ src/scripts/directives/piece.js
@@ -8,26 +8,30 @@
             return {
                 restrict: 'AE',
                 replace: 'true',
-                template: '<div class=\'piece\'></div>',
+                template: '<div class=\'piece\' ng-class=\'\'></div>',
                 scope: {
                     item: '@'
                 },
                 link: function(scope, el) {
 
-                    /*
-                                        scope.$observe('item', function(newVal) {
-                                            console.log(newVal);
-                                        });
-                    */
-                    var evalItem = scope.$eval(scope.item);
+                    function render() {
 
-                    $(el).click(function() {
-                        if (scope.$parent.checkAvailability(evalItem.line, evalItem.column)) {
-                            $(this).addClass('selected');
-                            $(this).css({
-                                backgroundColor: '#66aaaa'
+                        var evalItem = scope.$eval(scope.item);
+
+                        if (!evalItem.available) {
+                            $(el).addClass('selected');
+                            $(el).css({
+                                backgroundColor: evalItem.player.color
                             });
                         }
+
+                    }
+
+                    //key point here to watch for changes of the type property
+                    scope.$watch('item', function(newValue, oldValue) {
+                        console.log(newValue);
+                        console.log(oldValue);
+                        render();
                     });
                 }
             };
